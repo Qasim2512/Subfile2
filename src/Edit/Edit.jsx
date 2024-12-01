c5185c2982f74b1e99cb6c3803b4d37de58a285a
Qasim2512 <122743097+Qasim2512@users.noreply.github.com>
2024-12-01 22:48:23 +0100

    fixes2

--- src/Edit/Edit.jsx
+++ src/Edit/Edit.jsx
@@ -1,36 +1,62 @@
 /** @format */
 
 import "./Edit.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Edit() {
   const location = useLocation();
+  const navigate = useNavigate();
 
-  console.log(location);
+  const [title, setTitle] = useState(location.state.data.title);
+  const [description, setDescription] = useState(location.state.data.title);
+
+  const [titleError, setTitleError] = useState("");
+  const [descriptionError, setDescriptionError] = useState("");
+
+  function handleUpload() {
+    if (title.trim().length === 0) {
+      setTitleError("Write in the title");
+      return;
+    } else {
+      setTitleError("");
+    }
+
+    if (description.trim().length === 0) {
+      setDescriptionError("Please write a description of picture");
+      return;
+    } else {
+      setDescriptionError("");
+    }
+
+    navigate("/scrollView");
+  }
 
   return (
     <div className="EditMain">
       <div>
         <div>
           <h2>Edit Image {location.state.data.title}</h2>
-          <form method="post" className="FormMain">
+          <form method="#" className="FormMain">
             <input type="hidden" />
             <div className="form-group">
               <label>Title</label>
               <input
                 className="form-control"
-                defaultValue={location.state.data.title}
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
-              <span className="text-danger"></span>
+              <span className="text-danger">{titleError}</span>
             </div>
             <div className="form-group">
               <label>Description</label>
               <input
                 className="form-control"
-                defaultValue={location.state.data.title}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               />
-              <span className="text-danger"></span>
+              <span className="text-danger">{descriptionError}</span>
             </div>
             <div className="form-group">
               <label>Current Image:</label>
@@ -38,11 +64,13 @@ function Edit() {
               <img src={location.state.data.thumbnailUrl} alt="Current Image" />
             </div>
             <div className="buttons">
-              <a href="/scrollView">
-                <button type="button" className="btn btn-primary">
-                  Update
-                </button>
-              </a>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleUpload}
+              >
+                Update
+              </button>
               <a href="/scrollView" class="btn btn-secondary">
                 Cancel
               </a>
